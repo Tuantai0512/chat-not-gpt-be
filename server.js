02d093be98cf561f617cfbc791c71f4e222d1959
dev-peter <tuantai0512@gmail.com>
2023-05-05 10:36:39 +0700

    Setup environment for the server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 8000
-
+require('dotenv').config()
+const port = process.env.PORT || 8888
+const hostname = process.env.HOST_NAME
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port,hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })

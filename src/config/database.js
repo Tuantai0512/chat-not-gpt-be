fe32bf7df56c3e12a6c705c6a9119dab22bb5ab1
dev-peter <tuantai0512@gmail.com>
2023-05-16 18:44:14 +0700

    Installed and connected database by sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
-const mysql = require('mysql2');
+/* const mysql = require('mysql2'); */
+const { Sequelize } = require('sequelize');
 
 // create the connection to database
-const connection = mysql.createPool({
+/* const connection = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
@@ -13,6 +14,12 @@ const connection = mysql.createPool({
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
   queueLimit: 0
+}); */
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql', /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+  port: '3307'
 });
 
-module.exports = connection;
+module.exports = sequelize;
--- src/server.js
+++ src/server.js
@@ -5,15 +5,24 @@ var path = require('path');
 const port = process.env.PORT || 8888
 const hostname = process.env.HOST_NAME
 const webRoutes = require('./routes/web')
-const connection = require('./config/database');
+const sequelize = require('./config/database')
+/* const connection = require('./config/database');
 
 connection.execute(
   'SELECT * FROM Users',
   function(err, results, fields) {
     console.log(results); // results contains rows returned by server
   }
-);
+); */
 
+sequelize
+ .authenticate()
+ .then(() => {
+  console.info('INFO - Database connected.')
+ })
+ .catch(err => {
+  console.error('ERROR - Unable to connect to the database:', err)
+ })
 
 app.use(express.static(path.join(__dirname, 'public')));
 

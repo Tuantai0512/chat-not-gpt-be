de88d747066d3a855b36d050dde57f7cb5b46eb3
dev-peter <tuantai0512@gmail.com>
2023-05-21 17:18:19 +0700

    Install and config CORS policy accept frontend application pi

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,12 +2,21 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models/index')
 const APIControler = require('../controllers/APIControlers')
+var cors = require('cors')
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+
 
 // middleware that is specific to this router
 const initAPI = (app) => {
     // define the home page route
     router.post('/login', APIControler.handleLogin)
-    return app.use('/api/v1', router)
+    return app.use('/api/v1', cors(corsOptions) ,router)
 }
 
 // define the about route
